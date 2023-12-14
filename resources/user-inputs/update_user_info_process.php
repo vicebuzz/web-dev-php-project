8102db8b6c29c20650801e3f00ea12d44bd6b4a1
Wilhaz <113859172+Wilhaz@users.noreply.github.com>
2023-12-14 12:15:38 +0000

    Added error handling for User Input Validation

--- resources/user-inputs/update_user_info_process.php
+++ resources/user-inputs/update_user_info_process.php
@@ -3,43 +3,42 @@ require_once "../crud/userCrud.php";
 
 $updateType = $_POST["update-type"];
 
-function getTypeToChange ($valueChangeType){
-    $newEmail =$_POST["email"];
-    $newPassword=$_POST["password"];
-    $newUsername=$_POST["username"];
-    $newPhoneNum=$_POST["phone"];
+function getTypeToChange($valueChangeType)
+{
+    $newEmail = $_POST["email"];
+    $newPassword = $_POST["password"];
+    $newUsername = $_POST["username"];
+    $newPhoneNum = $_POST["phone"];
 
-    if ($valueChangeType=="update-username"){
-        updateUserInfo($newUsername,"username");
-    }
-    elseif ($valueChangeType=="update-password"){
-        updateUserInfo($newPassword,"password");
-    }
-    elseif ($valueChangeType=="update-email"){
+    if ($valueChangeType == "update-username" && !empty($newUsername)) {
+        updateUserInfo($newUsername, "username");
+    } elseif ($valueChangeType == "update-password" && !empty($newPassword)) {
+        updateUserInfo($newPassword, "password");
+    } elseif ($valueChangeType == "update-email" && !empty($newEmail)) {
         updateUserInfo($newEmail, "email");
-    }
-    elseif ($valueChangeType=="update-phone"){
+    } elseif ($valueChangeType == "update-phone" && !empty($newPhoneNum)) {
         updateUserInfo($newPhoneNum, "phoneNumber");
+    } else {
+        // Empty field detected, send alert back to the page
+        echo '<script>alert("Fields cannot be empty"); window.history.back();</script>';
+        exit();
     }
 }
 
-
-function updateUserInfo($newUserInput,$flag)
+function updateUserInfo($newUserInput, $flag)
 {
     session_start();
     $userCRUD = new UserCRUD();
 
     $paramArray = array(
-      array("userID"=>$_SESSION["userID"]),
-      array($flag=>$newUserInput)
+        array("userID" => $_SESSION["userID"]),
+        array($flag => $newUserInput)
     );
     $jsonArray = json_encode($paramArray);
     $userCRUD->updateUser($jsonArray);
     header("location: ../../public/login.php");
     exit();
-
 }
 
 getTypeToChange($updateType);
-
 ?>
